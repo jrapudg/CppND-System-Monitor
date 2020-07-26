b748cc6a67f17e8baca5beff9310ecbcaff576a2
Juan Alvarez <jrap.udg@gmail.com>
2020-07-26 23:04:02 +0000

    Add CPU utilization funciton

--- src/process.cpp
+++ src/process.cpp
@@ -17,8 +17,14 @@ Process::Process(int pid) : pid_(pid) {}
 // Return this process's ID
 int Process::Pid() { return Process::pid_; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// Return this process's CPU utilization
+float Process::CpuUtilization() { 
+    long pidTotalTime = LinuxParser::ActiveJiffies(Process::Pid());
+    long pidUpTime = LinuxParser::UpTime(Process::Pid());
+    float Hertz = sysconf(_SC_CLK_TCK); 
+    float cpuUsage = 100 * ((pidTotalTime/Hertz)/pidUpTime);
+    return cpuUsage; 
+}
 
 // TODO: Return the command that generated this process
 string Process::Command() { return string(); }
@@ -26,11 +32,11 @@ string Process::Command() { return string(); }
 // TODO: Return this process's memory utilization
 string Process::Ram() { return string(); }
 
-// TODO: Return the user (name) that generated this process
+// Return the user (name) that generated this process
 string Process::User() { return LinuxParser::User(Process::Pid()); }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// Return the age of this process (in seconds)
+long int Process::UpTime() { return LinuxParser::UpTime(Process::Pid()); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
