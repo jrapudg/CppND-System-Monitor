85eb1537b7d7749bc7dc5071f7df1e1f0764e262
Juan Alvarez <jrap.udg@gmail.com>
2020-07-26 20:23:29 +0000

    Add comments

--- include/format.h
+++ include/format.h
@@ -3,10 +3,10 @@
 
 #include <string>
 
-std::string FormatHelper(int num);
+std::string FormatHelper(int num); // Helper to ensure 2 digits format
 
 namespace Format {
-std::string ElapsedTime(long times);  // TODO: See src/format.cpp
+std::string ElapsedTime(long times);  // Format seconds to HH::MM::SS
 };                                    // namespace Format
 
 
--- src/format.cpp
+++ src/format.cpp
@@ -1,14 +1,10 @@
 #include <string>
-
 #include "format.h"
 
 using std::string;
 
-
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
 string FormatHelper(int num){
     if (num < 10)
         return "0" + std::to_string(num);
@@ -19,15 +15,14 @@ string FormatHelper(int num){
 string Format::ElapsedTime(long seconds) { 
     int hours, mins, sec;
     string HH, MM, SS;
-
+    // Calculate seconds, minutes and hours from seconds period
     sec = seconds % 60;
     mins = seconds / 60;
     hours = mins / 60;
     mins = mins % 60;
-
+    // Change to two digits string format 
     HH = FormatHelper(hours);
     MM = FormatHelper(mins);
     SS = FormatHelper(sec);
-
     return HH + ":" + MM + ":" + SS; 
 }
